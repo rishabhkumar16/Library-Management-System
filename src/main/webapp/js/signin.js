b5583abb818d47dc8b5b1e51cee2dcd3c7426176
AkshitaNayak <nayakakshita5@gmail.com>
2022-06-01 21:21:43 +0530

    Created fields for signin and signup in frontend

--- src/main/webapp/js/signin.js
+++ src/main/webapp/js/signin.js
@@ -1,4 +1,7 @@
 function signIn() {
-    var name = document.getElementById("username").value;
-    console.log(name);
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    var password = document.getElementById("password").value;
+    console.log(password);
 }
--- src/main/webapp/js/signup.js
+++ src/main/webapp/js/signup.js
@@ -1,4 +1,15 @@
 function signUp() {
     var name = document.getElementById("username").value;
     console.log(name);
+
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    var password = document.getElementById("password").value;
+    console.log(password);
+
+    var number = document.getElementById("number").value;
+    console.log(number);
+
+    
 }
