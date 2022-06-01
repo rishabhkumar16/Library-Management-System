6978ea1ae8dfdee7a86e017069be2ba0c6fd6594
AkshitaNayak <nayakakshita5@gmail.com>
2022-06-01 23:10:38 +0530

    Created addBook form in html, css and js

---
+++ src/main/webapp/js/addbook.js
@@ -0,0 +1,18 @@
+function addBook() {
+    var title = document.getElementById("title").value;
+    console.log(title);
+
+    var isbn = document.getElementById("isbn").value;
+    console.log(isbn);
+
+    var edition = document.getElementById("edition").value;
+    console.log(edition);
+
+    var author = document.getElementById("author").value;
+    console.log(author);
+
+    var price = document.getElementById("price").value;
+    console.log(price);
+
+    
+}
