8dbd5e92cc4b9551b1fbc7528e681e533e7f940e
AkshitaNayak <nayakakshita5@gmail.com>
2022-06-04 12:07:26 +0530

    Issue book form in html,css and js

---
+++ src/main/webapp/js/issuebook.js
@@ -0,0 +1,24 @@
+function issueBook() {
+
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    var name = document.getElementById("name").value;
+    console.log(name);
+
+    var userid = document.getElementById("userid").value;
+    console.log(userid);
+
+    var title = document.getElementById("title").value;
+    console.log(title);
+
+    var bookid = document.getElementById("bookid").value;
+    console.log(bookid);
+
+    var issuedate = document.getElementById("issuedate").value;
+    console.log(issuedate);
+
+    var expirydate = document.getElementById("expirydate").value;
+    console.log(expirydate);
+    
+}
