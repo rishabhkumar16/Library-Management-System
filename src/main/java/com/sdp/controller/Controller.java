440b1881ed9c718fdfea0629cff88ef2906e0b1d
amankr6316 <amanp6316@gmail.com>
2022-07-03 18:32:34 +0530

    signup changes

--- src/main/java/com/sdp/controller/Controller.java
+++ src/main/java/com/sdp/controller/Controller.java
@@ -2,6 +2,7 @@ package com.sdp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -18,8 +19,12 @@ public class Controller {
 		return "signup";
 	}
 	
-	@RequestMapping(value = "/user/save", method=RequestMethod.POST)
-	public String userSave() {
+	@RequestMapping(value = "/save", method=RequestMethod.POST)
+	public String userSave(@RequestParam("name") String name, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("number") String number) {
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+		System.out.println(number);
 		System.out.println("IN CONTROLLER userSave CLASS");
 		return "signin";
 	}
